6ff49d39dd375cf39b67af21749ef379493cb44c
Tommaso Bosetti <tbosetti@seac.it>
2025-08-26 16:39:03 +0200

    End of section 3

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,6 @@ namespace API.Entities;
 public class AppUser
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    public required string DispalyName { get; set; }
+    public required string DisplayName { get; set; }
     public required string Email { get; set; }
 }
---
+++ client/src/app/app.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating app';
+  protected members = signal<any>([]);
+
+  //così è utilizzare subscribe
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Richiesta http completata')
+    })
+  }
+
+  /*
+  Così sarebbe usare i promise
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error)
+      throw error;
+    }
+  }
+  */
+
+}
