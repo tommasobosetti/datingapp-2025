26c2f19867e379c6a8118a9e1134c39ecbc745a2
Tommaso Bosetti <tbosetti@seac.it>
2025-08-28 15:10:05 +0200

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")] // /api/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await EmailExists(registerDto.Email))
+            return BadRequest("Email già utilizzata");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user.ToDto(tokenService);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+        if (user == null)
+            return Unauthorized("Email non valida");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Password non valida");
+        }
+
+        return user.ToDto(tokenService);
+    }
+
+    /*
+    Questo metodo restituisce AppUser, che però contiene anche la password hashato e il salt e non ha senso. 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+            if (user == null)
+                return Unauthorized("Email non valida");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Password non valida");
+            }
+
+            return user;
+        }
+    */
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Nessun Token Key");
+        if (tokenKey.Length < 64)
+            throw new Exception("La Token Key deve essere >= di 64 caratteri");
+
+        // chiave per firmare il token. Simmetrica perchè questa chiave viene usata sia per crittare che per decrittare 
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        // informazioni sull'utente che l'utente dichiara su se stesso. 
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.NameIdentifier, user.Id)
+        };
+
+        // Credenziali che si basano sulla chiave generata prima per firmare il token
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        // Queste sono le proprietà e caratteristiche che il token dovrà avere
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7), // solitamente molto corte, si parla di minuti
+            SigningCredentials = creds
+        };
+
+        // Questa classe crea il token sulla base del descrittore qui sopra. 
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
