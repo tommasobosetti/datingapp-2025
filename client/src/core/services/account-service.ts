5236c853a6cf900d9ca9965202f4e389390712ce
Tommaso Bosetti <tbosetti@seac.it>
2025-08-29 15:53:27 +0200

    lesson 5.51

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,15 +1,30 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
 
   baseurl = 'https://localhost:5001/api/';
 
   login(creds: any) {
-    return this.http.post(this.baseurl + 'account/login', creds);
+    return this.http.post<User>(this.baseurl + 'account/login', creds).pipe(
+      tap(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
