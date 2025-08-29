a6867475c958d9938345ad5204b1e4c6216f12a9
Tommaso Bosetti <tbosetti@seac.it>
2025-08-29 16:00:56 +0200

    lesson 5.52

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +11,33 @@ import { Nav } from "../layout/nav/nav";
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'Dating app';
   protected members = signal<any>([]);
 
   //così è utilizzare subscribe
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: error => console.log(error),
-      complete: () => console.log('Richiesta http completata')
-    })
-  }
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:5001/api/members').subscribe({
+  //     next: response => this.members.set(response),
+  //     error: error => console.log(error),
+  //     complete: () => console.log('Richiesta http completata')
+  //   })
+  // }
 
-  /*
-  Così sarebbe usare i promise
+  //Così sarebbe usare i promise
   async ngOnInit() {
     this.members.set(await this.getMembers())
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString)
+      return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
@@ -36,6 +48,5 @@ export class App implements OnInit {
       throw error;
     }
   }
-  */
 
 }
