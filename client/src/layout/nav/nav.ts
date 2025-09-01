235d3abd3c9360d263e84ec482e050e208b29e38
Tommaso Bosetti <tbosetti@seac.it>
2025-09-01 13:59:30 +0200

    lesson 6.63-6.64

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -12,15 +13,19 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 export class Nav {
   protected accountService = inject(AccountService);
   private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {}
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         this.router.navigateByUrl('/members');
+        this.toast.success("Login effettuato correttamente");
         this.creds = {};
       },
-      error: error => alert(error.message),
+      error: error => {
+        this.toast.error(error.error);
+      },
     });
   }
 
