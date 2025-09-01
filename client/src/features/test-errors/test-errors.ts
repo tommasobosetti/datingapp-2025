27eaf80e10e35bb166ce489beee55ffb4479a662
Tommaso Bosetti <tbosetti@seac.it>
2025-09-01 15:36:23 +0200

    lesson 7.73

---
+++ client/src/features/test-errors/test-errors.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  imports: [],
+  templateUrl: './test-errors.html',
+  styleUrl: './test-errors.css'
+})
+export class TestErrors {
+  private http = inject(HttpClient);
+  baseurl = 'https://localhost:5001/api/';
+
+  get404Error() {
+    this.http.get(this.baseurl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseurl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get500Error() {
+    this.http.get(this.baseurl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401Error() {
+    this.http.get(this.baseurl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseurl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+}
