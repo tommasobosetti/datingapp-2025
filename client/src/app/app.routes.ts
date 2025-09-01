671c18eef3960def51e11ed493ce670e4b4a2dd1
Tommaso Bosetti <tbosetti@seac.it>
2025-09-01 14:28:25 +0200

    lesson 6.66

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,9 +8,16 @@ import { authGuard } from '../core/guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'members', component: MemberList, canActivate: [authGuard] },
-    { path: 'members/:id', component: MemberDetailed },
-    { path: 'lists', component: Lists },
-    { path: 'messages', component: Messages },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberList },
+            { path: 'members/:id', component: MemberDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
     { path: '**', component: Home },
 ];
