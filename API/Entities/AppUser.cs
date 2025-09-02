a69c02417cf46a3d7916192a45c9d4daea9f6719
Tommaso Bosetti <tbosetti@seac.it>
2025-09-02 14:26:06 +0200

    lesson 8.81

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,8 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    //Duplicazione del dato per evitare una join ogni volta tra due tabelle. 
+    public string? ImageUrl { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
 }
