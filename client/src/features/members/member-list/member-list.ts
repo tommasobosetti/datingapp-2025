2b8c0bf8d72bdfeab8162bf0d6b97b0aff95d2d5
Tommaso Bosetti <tbosetti@seac.it>
2025-09-03 15:01:32 +0200

    lesson 9.94

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -3,10 +3,11 @@ import { MemberService } from '../../../core/services/member-service';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AsyncPipe } from '@angular/common';
+import { MemberCard } from "../member-card/member-card";
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MemberCard],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
