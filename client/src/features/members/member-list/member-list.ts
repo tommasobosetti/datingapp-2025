dbeb838e39612b25e792edbb60ea77d40094885f
Tommaso Bosetti <tbosetti@seac.it>
2025-09-03 14:50:37 +0200

    lesson 9.93

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { Observable } from 'rxjs';
+import { Member } from '../../../types/member';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
 export class MemberList {
+  private memberService = inject(MemberService);
+  protected members$: Observable<Member[]>;
 
+  constructor() {
+    this.members$ = this.memberService.getMembers();
+  }
 }
