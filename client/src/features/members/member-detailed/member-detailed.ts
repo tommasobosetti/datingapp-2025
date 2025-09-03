0a0574198ad05fa46b7cde16a0465a6ecf68a396
Tommaso Bosetti <tbosetti@seac.it>
2025-09-03 15:20:12 +0200

    lesson 9.97

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css'
 })
-export class MemberDetailed {
+export class MemberDetailed implements OnInit {
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
+  protected member$?: Observable<Member>;
+
+  ngOnInit(): void {
+    this.member$ = this.loadMember();
+  }
+
+  loadMember() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id)
+      return;
+
+    return this.memberService.getMember(id);
+  }
 
 }
