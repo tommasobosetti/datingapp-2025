63d61ca17b4b837410d2fabd34e331fe585cae1a
Tommaso Bosetti <tbosetti@seac.it>
2025-09-05 13:52:53 +0200

    lesson 10.112

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -29,6 +31,32 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (memberId == null)
+                return BadRequest("Oops - no id found in token");
+
+            var member = await memberRepository.GetMemberByIdAsync(memberId);
+
+            if (member == null)
+                return BadRequest("Could not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            memberRepository.Update(member);
+
+            if (await memberRepository.SaveAllAsync())
+                return NoContent();
+
+            return BadRequest("Failed to update member");
         }
     }
 }
