ff94fecfbb83e170a004f30c4035ef4eba1263ee
Tommaso Bosetti <tbosetti@seac.it>
2025-09-11 14:36:18 +0200

    lesson 11.128

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -92,5 +92,27 @@ namespace API.Controllers
 
             return BadRequest("Problem adding photo");
         }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if (member == null)
+                return BadRequest("Cannot get member from token");
+
+            var photo = member.Photos.SingleOrDefault(x => x.Id == photoId);
+
+            if (member.ImageUrl == photo?.Url || photo == null)
+                return BadRequest("Cannot set this as main image");
+
+            member.ImageUrl = photo.Url;
+            member.User.ImageUrl = photo.Url;
+
+            if (await memberRepository.SaveAllAsync())
+                return NoContent();
+
+            return BadRequest("Problem setting main photo");
+        }
     }
 }
