a69e119e7ab2ace3200d32263137e816d6724dc0
Tommaso Bosetti <tbosetti@seac.it>
2025-09-11 16:16:43 +0200

    lesson 11.131

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -114,5 +114,34 @@ namespace API.Controllers
 
             return BadRequest("Problem setting main photo");
         }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if (member == null)
+                return BadRequest("Cannot get member from token");
+
+            var photo = member.Photos.SingleOrDefault(x => x.Id == photoId);
+
+            if (photo == null || photo.Url == member.ImageUrl)
+                return BadRequest("This photo cannot be deleted");
+
+            if (photo.PublicId != null)
+            {
+                var result = await photoService.DeletePhotoAsync(photo.PublicId);
+
+                if (result.Error != null)
+                    return BadRequest(result.Error.Message);
+            }
+
+            member.Photos.Remove(photo);
+
+            if (await memberRepository.SaveAllAsync())
+                return Ok();
+
+            return BadRequest("Problem deleting the photo");
+        }
     }
 }
