657ea3e588828c5532dd97eef9185f87ea38bc29
Tommaso Bosetti <tbosetti@seac.it>
2025-09-11 11:23:15 +0200

    lesson 11.125

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -4,15 +4,16 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../../../types/member';
 import { AsyncPipe } from '@angular/common';
+import { ImageUpload } from "../../../shared/image-upload/image-upload";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ImageUpload],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos {
-  private memberService = inject(MemberService);
+  protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   protected photos$?: Observable<Photo[]>;
 
---
+++ client/src/shared/image-upload/image-upload.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-upload',
+  imports: [],
+  templateUrl: './image-upload.html',
+  styleUrl: './image-upload.css'
+})
+export class ImageUpload {
+
+}
