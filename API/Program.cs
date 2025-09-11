0e2bb717ad7aa8cecdf48bb671befd0620f245c5
Tommaso Bosetti <tbosetti@seac.it>
2025-09-11 09:24:29 +0200

    lesson 11.123

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
