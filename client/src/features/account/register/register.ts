18ac94c7f8cda2d085495bafd580f585796008a2
Tommaso Bosetti <tbosetti@seac.it>
2025-09-12 16:55:49 +0200

    lesson 12.143

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -61,6 +61,12 @@ export class Register {
     this.currentStep.update(prevStep => prevStep - 1)
   }
 
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split('T')[0];
+  }
+
   register() {
     if (this.profileForm.valid && this.credentialsForm.valid) {
       const formData = { ...this.credentialsForm.value, ...this.profileForm.value };
