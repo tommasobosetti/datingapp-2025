9dc97d7a3bdcccea68a9fb088b1df0a9ece8efdd
Tommaso Bosetti <tbosetti@seac.it>
2025-09-15 11:33:21 +0200

    lesson 13.154

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
@@ -13,18 +13,22 @@ import { Paginator } from "../../../shared/paginator/paginator";
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
-export class MemberList {
+export class MemberList implements OnInit {
   private memberService = inject(MemberService);
-  protected paginatedMembers$?: Observable<PaginatedResult<Member>>;
+  protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
   pageNumber = 1;
   pageSize = 5;
 
-  constructor() {
+  ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers() {
-    this.paginatedMembers$ = this.memberService.getMembers(this.pageNumber, this.pageSize);
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: result => {
+        this.paginatedMembers.set(result);
+      }
+    });
   }
 
   onPageChange(event: { pageNumber: number, pageSize: number }) {
