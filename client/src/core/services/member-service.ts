79418dcf5315d908e18f12f80ccabae1740d9c5a
Tommaso Bosetti <tbosetti@seac.it>
2025-09-15 10:58:08 +0200

    lesson 13.151

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { EditableMember, Member, Photo } from '../../types/member';
 import { tap } from 'rxjs';
+import { PaginatedResult } from '../../types/pagination';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,13 @@ export class MemberService {
   editMode = signal(false);
   member = signal<Member | null>(null);
 
-  getMembers() {
-    // return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
-    return this.http.get<Member[]>(this.baseUrl + 'members');
+  getMembers(pageNumber = 1, pageSize = 5) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+
+    return this.http.get<PaginatedResult<Member>>(this.baseUrl + 'members', { params });
   }
 
   getMember(id: string) {
