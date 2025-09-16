1da321eff4869db4f7d16e4207444895df804d93
Tommaso Bosetti <tbosetti@seac.it>
2025-09-16 16:08:58 +0200

    lesson 14.172

--- API/Data/LikeRepository.cs
+++ API/Data/LikeRepository.cs
@@ -9,30 +9,41 @@ public class LikeRepository(AppDbContext context) : ILikesRepository
 {
     public void AddLike(MemberLike like)
     {
-        context.Entry(like).State = EntityState.Added;
+        context.Likes.Add(like);
     }
 
     public void DeleteLike(MemberLike like)
     {
-        context.Entry(like).State = EntityState.Deleted;
+        context.Likes.Remove(like);
     }
 
     public async Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId)
     {
         return await context.Likes
                             .Where(x => x.SourceMemberId == memberId)
-                            .SelectMany(x => x.TargetMemberId)
+                            .Select(x => x.TargetMemberId)
                             .ToListAsync();
     }
 
     public async Task<MemberLike?> GetMemberLike(string sourceMemberId, string targetMemberId)
     {
-        return await context.Likes.SingleOrDefaultAsync(x => x.SourceMemberId == sourceMemberId && x.TargetMemberId == targetMemberId);
+        return await context.Likes.FindAsync(sourceMemberId, targetMemberId);
     }
 
-    public Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId)
+    public async Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId)
     {
-        throw new NotImplementedException();
+        var query = context.Likes.AsQueryable();
+
+        switch (predicate)
+        {
+            case "liked":
+                return await query.Where(x => x.SourceMemberId == memberId).Select(x => x.TargetMember).ToListAsync();
+            case "liked by":
+                return await query.Where(x => x.TargetMemberId == memberId).Select(x => x.SourceMember).ToListAsync();
+            default: // mutual
+                var likeIds = await GetCurrentMemberLikeIds(memberId);
+                return await query.Where(x => x.TargetMemberId == memberId && likeIds.Contains(x.SourceMemberId)).Select(x => x.SourceMember).ToListAsync();
+        }
     }
 
     public async Task<bool> SaveAllChanges()
