a6706775871dd958fbe60ec0fa7a391bf284948e
Tommaso Bosetti <tbosetti@seac.it>
2025-09-16 11:12:20 +0200

    lesson 13.163

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -17,6 +17,7 @@ export class MemberList implements OnInit {
   private memberService = inject(MemberService);
   protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
   protected memberParams = new MemberParams();
+  private updatedParams = new MemberParams();
 
   ngOnInit(): void {
     this.loadMembers();
@@ -45,12 +46,33 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams) {
-    this.memberParams = data;
+    this.memberParams = { ...data };
+    this.updatedParams = { ...data };
     this.loadMembers();
   }
 
   resetFilter() {
     this.memberParams = new MemberParams();
     this.loadMembers();
+  }
+
+  get displayMessage(): string {
+    const defaultParams = new MemberParams();
+
+    const filters: string[] = [];
+
+    if (this.updatedParams.gender) {
+      filters.push(this.updatedParams.gender + 's');
+    }
+    else
+      filters.push('Males, Females');
+
+    if (this.updatedParams.minAge !== defaultParams.minAge || this.updatedParams.maxAge !== defaultParams.maxAge) {
+      filters.push(` ages ${this.updatedParams.minAge}-${this.updatedParams.maxAge}`);
+    }
+
+    filters.push(this.updatedParams.orderBy === 'lastActive' ? 'Recently active' : 'Newest members');
+
+    return filters.length > 0 ? `Selected: ${filters.join('  | ')}` : 'All members';
   }
 }
