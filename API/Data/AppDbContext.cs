64a21175e398c7a2959c2f5aa50e9d3adc5af81c
Tommaso Bosetti <tbosetti@seac.it>
2025-09-16 15:43:17 +0200

    lesson 14.170

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -10,10 +10,25 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
+    public DbSet<MemberLike> Likes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MemberLike>().HasKey(x => new { x.SourceMemberId, x.TargetMemberId });
+
+        modelBuilder.Entity<MemberLike>()
+                    .HasOne(s => s.SourceMember)
+                    .WithMany(t => t.LikedMembers)
+                    .HasForeignKey(s => s.SourceMemberId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<MemberLike>()
+                    .HasOne(s => s.TargetMember)
+                    .WithMany(t => t.LikedByMembers)
+                    .HasForeignKey(s => s.TargetMemberId)
+                    .OnDelete(DeleteBehavior.NoAction); //SQL Server non vuole avere il deleteCascade su entrambi i lati della relazione
 
         var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
             v => v.ToUniversalTime(),
