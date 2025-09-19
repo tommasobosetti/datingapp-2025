2d011f323a27d46902fe2dd3a4259720bdd78d64
Tommaso Bosetti <tbosetti@seac.it>
2025-09-19 10:06:18 +0200

    lesson 15.188

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MessageService } from '../../core/services/message-service';
+import { PaginatedResult } from '../../types/pagination';
+import { Message } from '../../types/message';
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
-export class Messages {
+export class Messages implements OnInit {
+  private messageService = inject(MessageService);
+  protected container = 'Inbox';
+  protected pageNumber = 1;
+  protected pageSize = 10;
+  protected paginatedMessages = signal<PaginatedResult<Message> | null>(null);
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.container, this.pageNumber, this.pageSize).subscribe({
+      next: response => this.paginatedMessages.set(response)
+    });
+  }
 }
