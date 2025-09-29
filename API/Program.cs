bc8e578d52a05cefbba750481b2d8869deacbcce
Tommaso Bosetti <tbosetti@seac.it>
2025-09-29 15:50:32 +0200

    lesson 17.219

--- API/Program.cs
+++ API/Program.cs
@@ -50,6 +50,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrWhiteSpace(accessToken) && path.StartsWithSegments("/hubs"))
+                            {
+                                context.Token = accessToken;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
 
 builder.Services.AddAuthorizationBuilder()
