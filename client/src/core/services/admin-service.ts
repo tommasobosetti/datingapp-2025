3d65b980d15467fa50d348e9b102d2b5766cb68a
Tommaso Bosetti <tbosetti@seac.it>
2025-09-29 12:20:20 +0200

    lesson 16.212

--- client/src/core/services/admin-service.ts
+++ client/src/core/services/admin-service.ts
@@ -13,4 +13,8 @@ export class AdminService {
   getUserWithRoles() {
     return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
   }
+
+  updateUserRoles(userId: string, roles: string[]) {
+    this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/' + userId + '?roles=' + roles, {});
+  }
 }
--- client/src/features/admin/user-management/user-management.ts
+++ client/src/features/admin/user-management/user-management.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { AdminService } from '../../../core/services/admin-service';
 import { User } from '../../../types/user';
 
@@ -9,8 +9,11 @@ import { User } from '../../../types/user';
   styleUrl: './user-management.css'
 })
 export class UserManagement implements OnInit {
+  @ViewChild('rolesModal') rolesModal!: ElementRef<HTMLDialogElement>;
   private adminService = inject(AdminService);
   protected users = signal<User[]>([]);
+  protected availableRoles = ['Member', 'Moderator', 'Admin'];
+  protected selectedUser: User | null = null;
 
   ngOnInit(): void {
     this.getUserWithRoles();
@@ -20,5 +23,10 @@ export class UserManagement implements OnInit {
     this.adminService.getUserWithRoles().subscribe({
       next: users => this.users.set(users)
     })
+  }
+
+  openRolesModal(user: User) {
+    this.selectedUser = user;
+    this.rolesModal.nativeElement.showModal();
   }
 }
