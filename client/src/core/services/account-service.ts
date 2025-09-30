69af8b19713cfb8e1ea410626fc8bba2a7600b11
Tommaso Bosetti <tbosetti@seac.it>
2025-09-30 11:17:49 +0200

    lesson 18.235

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -67,10 +67,15 @@ export class AccountService {
   }
 
   logout() {
-    localStorage.removeItem('filters');
-    this.likesService.clearLikeIds();
-    this.currentUser.set(null);
-    this.presenceService.stopHubConnection();
+    this.http.post(this.baseUrl + 'account/logout', {}, { withCredentials: true }).subscribe({
+      next: () => {
+        localStorage.removeItem('filters');
+        this.likesService.clearLikeIds();
+        this.currentUser.set(null);
+        this.presenceService.stopHubConnection();
+      }
+    });
+
   }
 
   private getRolesFromToken(user: User): string[] {
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -21,6 +21,7 @@ export class Nav implements OnInit {
   protected creds: any = {}
   protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
   protected themes = themes;
+  protected loading = signal(false);
 
   ngOnInit(): void {
     document.documentElement.setAttribute('data-theme', this.selectedTheme());
@@ -36,6 +37,7 @@ export class Nav implements OnInit {
   }
 
   login() {
+    this.loading.set(true);
     this.accountService.login(this.creds).subscribe({
       next: () => {
         this.router.navigateByUrl('/members');
@@ -45,6 +47,7 @@ export class Nav implements OnInit {
       error: error => {
         this.toast.error(error.error);
       },
+      complete: () => this.loading.set(false)
     });
   }
 
