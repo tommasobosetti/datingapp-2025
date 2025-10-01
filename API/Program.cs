42da6d8c82b295c2229b057df849c7665caa0c72
Tommaso Bosetti <tbosetti@seac.it>
2025-10-01 11:17:56 +0200

    lesson 19.246

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
